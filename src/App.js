fb473cfbc92d16970d03209eddf3b9e90706ba38
Sulyman Mariam Tinuola <sulymanmariam88@gmail.com>
2022-06-30 10:55:19 +0100

    classwork

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 
 import { useState } from 'react'
-import History from './History'
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const hello = () => {
+    const handler = () => console.log('hello world')
+    return handler
   }
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
-      <History allClicks={allClicks}/>
+      {value}
+      <button onClick={hello()}>button</button>
     </div>
   )
 }
-
 export default App
